5978138b63d71463f29e74cb31dde4812c1b2efb
SebaRodriM19 <50354922+SebaRodriM19@users.noreply.github.com>
2023-03-16 14:57:17 +0100

    Add files via upload
    Change name

---
+++ Runner.cs
@@ -0,0 +1,13 @@
+﻿namespace Sample;
+
+public class Runner
+{
+    private readonly Func<string, bool> _sender;
+
+    public Runner(Func<string, bool> sender)
+    {
+        _sender = sender;
+    }
+
+    public void Run() => _sender("SebaRodri");
+}
